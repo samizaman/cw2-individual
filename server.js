f3cca5803ebd6ec071bfc84ab4513ab060b6b320
samizaman <sxmizaman@gmail.com>
2023-02-08 13:39:57 +0400

    removed redundant code

--- server.js
+++ server.js
@@ -30,10 +30,6 @@ app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(`${new Date().toLocaleTimeString()} - ${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-
   next();
 });
 
